59d704f0fddd209cde1c938ad6b01074882a92da
trey-rosius <treyrosius@gmail.com>
2022-08-12 09:21:02 +0100

    first commit

---
+++ functions/lambda/app.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+
+step_function_client = boto3.client("stepfunctions")
+
+
+def lambda_handler(event, context):
+    print("Lambda function invoked")
+    print(json.dumps(event))
+    print(json.dumps(event["arguments"]['input']))
+    step_function_client.start_execution(
+        stateMachineArn=event["arguments"]['input']['arn'],
+        name=event["arguments"]['input']['id'],
+        input= "{\"details\":{\"accountId\":\"1234567\",\"bookedStatus\":\"Booked\"}}",
+
+    )
+
+    return {"id": event["arguments"]['input']['id'], "arn": event["arguments"]['input']['arn']}
